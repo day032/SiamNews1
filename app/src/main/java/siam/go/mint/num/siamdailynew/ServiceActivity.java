5ba75c1801c7b97be5664fbf95533ffc79cc1663
day <dacha.boonkang.gmail.com>
2017-08-22 15:47:26 +0700

    News FragmentOkแล้ว

---
+++ app/src/main/java/siam/go/mint/num/siamdailynew/ServiceActivity.java
@@ -0,0 +1,25 @@
+package siam.go.mint.num.siamdailynew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import siam.go.mint.num.siamdailynew.fragment.NewsFragment;
+
+public class ServiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service);
+        //AddFragment
+        if (savedInstanceState == null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.serviceContent, new NewsFragment())
+                    .commit();
+        }
+
+
+    }//main Method
+
+}//main class
